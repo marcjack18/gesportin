3dae231db2e71a45b3a0fd3c5a1f81a666b332ff
alan <alanalcaniz24@gmail.com>
2026-01-16 18:04:03 +0000

    OneToMany arreglo 2

--- src/main/java/net/ausiasmarch/gesportin/entity/CategoriaEntity.java
+++ src/main/java/net/ausiasmarch/gesportin/entity/CategoriaEntity.java
@@ -2,8 +2,6 @@ package net.ausiasmarch.gesportin.entity;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
